031495a4146026cb34ae499d560ddda8d028c7c4
Orest DEV-10 <orest@proficiencyconsulting.com>
2016-09-30 19:55:58 -0400

    Add Sort

--- Timetracker/Controllers/api/ProjectController.cs
+++ Timetracker/Controllers/api/ProjectController.cs
@@ -33,7 +33,7 @@ namespace Timetracker.Controllers.api
                 Project = p
             });
 
-            pr = pr.OrderByDescending(p => p.HasActiveLog).ThenByDescending( p => p.Project.Rate);
+            pr = pr.OrderByDescending(p => p.HasActiveLog).ThenByDescending( p => p.Project.Rate).ThenBy(p=>p.Project.ClientId);
             return Ok(pr);
         }
 
